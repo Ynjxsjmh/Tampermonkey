6fff92f37b5e1e4c1724fcde76b53bc699a97553
Ynjxsjmh <ynjxsjmh@gmail.com>
2019-12-22 20:46:51 +0800

    [Feature] Add support for disguising YouTube

--- Disguiser.js
+++ Disguiser.js
@@ -1,70 +1,117 @@
 // ==UserScript==
-// @name 把 google 搜索伪装成bing搜索
+// @name Disguiser
 // @namespace win.somereason.web.utils
-// @version 2018.12.10.2
-// @description （百度一生黑） Honor to @somereason
+// @version 2019.12.22
+// @description （百度一生黑） Honor to somereason
 // @author Ynjxsjmh
 // @license MIT
 // @date 2018-10-05
 // @match *://www.google.com/search*
 // @match *://www.google.com.hk/search*
 // @match *://www.google.com.tw/search*
+// @match *://www.youtube.com/*
 // @grant none
 // ==/UserScript==
 //
 
-
 (function () {
-	var logo = document.getElementById("logo");
-    var logoArr;
-	if (logo === null) {
-		logoArr = document.getElementsByClassName("logo");
-		if (logoArr.length > 0)
-			logo = logoArr[0];
-	}
-	if (logo === null) {
-		logoArr = document.getElementsByClassName("logocont");
-		if (logoArr.length > 0)
-			logo = logoArr[0];
-	}
-	if (logo === null) {
-		console.log("oops,google又改样式了.请静待更新");
-	} else {
-        logo.innerHTML = '';
-
-        var image_url = "https://raw.githubusercontent.com/Ynjxsjmh/Tampermonkey/master/image/bing-green-small.png";
-        var img = new Image();
-        img.src = image_url;
-
-        var imgSize = getImgSize(logo);
-        var percentage = imgSize.height/img.height;
-
-        logo.innerHTML = '<a href="https://www.bing.com" data-hveid="7"><img src="https://raw.githubusercontent.com/Ynjxsjmh/Tampermonkey/master/image/bing-green-small.png" alt="Bing" height="'+imgSize.height+'px" width="'+img.width*percentage+'px" data-atf="3"></a>';
-        document.title = document.title.replace(/Google\s/g, "必应");
-        // Below method has a slight delay while changing the logo
-        // document.getElementById("logo").getElementsByTagName('img')[0].src = image_url;
-
-        if (document.getElementById("logo") !== null) {
-            document.getElementById("logo").title = "Bing 首页";
+    try{
+        switch(window.location.hostname){
+            case "www.google.com":
+            case "www.google.com.hk":
+            case "www.google.com.tw":
+                google();
+                break;
+            case "www.youtube.com":
+            case "youtube.com":
+                youtube();
+                break;
+            case "instagram.com":
+                instagram();
+                break;
+            default:
+                throw TypeError;
         }
+    }catch(e){
+        console.log("unknown source");
+    }
 
-        // Don't use below line, or you cound't get your input while the cursor hovering onto the tab
-        // document.head.getElementsByTagName("title")[0].text = "微软 Bing 搜索 - 国际版";
+    function youtube() {
+        /* Change Logo  */
+        document.getElementById("logo").tooltipText = "Youku 优酷";
+        var logo = document.getElementById("logo").firstElementChild;
+        logo.title = "Youku 优酷";
+        logo.innerHTML = "";
+        logo.innerHTML = '<a href="https://www.youtube.com" data-hveid="7"><img src="https://raw.githubusercontent.com/Ynjxsjmh/Tampermonkey/master/image/youku.png" alt="Youku" data-atf="3"></a>';
 
-        // If you are using console and then type dot, you can get a prompot.
-        // With focus on one choice, there will have a pre-output
-        // I find `attributes`'s pre-output got that `aria-label`
-        document.getElementsByTagName('button')[0].attributes[2].value = "Bing 搜索";
+        /* Change title */
+        document.getElementsByTagName("title")[0].text = "优酷 - 这世界很酷";
 
-        document.getElementsByTagName('input')[1].title = "Bing 搜索";
+        /* Change shortcut icon */
+        /*
+          <link rel="shortcut icon" href="https://s.ytimg.com/yts/img/favicon-vfl8qSV2F.ico" type="image/x-icon" />
+          <link rel="icon" href="https://s.ytimg.com/yts/img/favicon_32-vflOogEID.png" sizes="32x32" />
+          <link rel="icon" href="https://s.ytimg.com/yts/img/favicon_48-vflVjB_Qk.png" sizes="48x48" />
+          <link rel="icon" href="https://s.ytimg.com/yts/img/favicon_96-vflW9Ec0w.png" sizes="96x96" />
+          <link rel="icon" href="https://s.ytimg.com/yts/img/favicon_144-vfliLAfaB.png" sizes="144x144" />
+        */
+        for (var i = 0; i < 5; i++) {
+            document.getElementsByTagName("link")[i].href = "https://raw.githubusercontent.com/Ynjxsjmh/Tampermonkey/master/image/youku-icon.png";
+        }
+    }
 
-        // change shortcut icon
-        document.getElementsByTagName('meta')[0].content = "https://raw.githubusercontent.com/Ynjxsjmh/Tampermonkey/master/image/bing-icon.ico";
-        var new_link = document.createElement("link");
-        new_link.href = "https://raw.githubusercontent.com/Ynjxsjmh/Tampermonkey/master/image/bing-icon.ico";
-        new_link.rel = "shortcut icon";
-        document.head.appendChild(new_link);
-	}
+    function google() {
+        var logo = document.getElementById("logo");
+        var logoArr;
+        if (logo === null) {
+            logoArr = document.getElementsByClassName("logo");
+            if (logoArr.length > 0)
+                logo = logoArr[0];
+        }
+        if (logo === null) {
+            logoArr = document.getElementsByClassName("logocont");
+            if (logoArr.length > 0)
+                logo = logoArr[0];
+        }
+        if (logo === null) {
+            console.log("oops, google又改样式了.请静待更新");
+        } else {
+            logo.innerHTML = '';
+
+            var image_url = "https://raw.githubusercontent.com/Ynjxsjmh/Tampermonkey/master/image/bing-green-small.png";
+            var img = new Image();
+            img.src = image_url;
+
+            var imgSize = getImgSize(logo);
+            var percentage = imgSize.height/img.height;
+
+            logo.innerHTML = '<a href="https://www.bing.com" data-hveid="7"><img src="https://raw.githubusercontent.com/Ynjxsjmh/Tampermonkey/master/image/bing-green-small.png" alt="Bing" height="'+imgSize.height+'px" width="'+img.width*percentage+'px" data-atf="3"></a>';
+            document.title = document.title.replace(/Google\s/g, "必应");
+            // Below method has a slight delay while changing the logo
+            // document.getElementById("logo").getElementsByTagName('img')[0].src = image_url;
+
+            if (document.getElementById("logo") !== null) {
+                document.getElementById("logo").title = "Bing 首页";
+            }
+
+            // Don't use below line, or you cound't get your input while the cursor hovering onto the tab
+            // document.head.getElementsByTagName("title")[0].text = "微软 Bing 搜索 - 国际版";
+
+            // If you are using console and then type dot, you can get a prompot.
+            // With focus on one choice, there will have a pre-output
+            // I find `attributes`'s pre-output got that `aria-label`
+            document.getElementsByTagName('button')[0].attributes[2].value = "Bing 搜索";
+
+            document.getElementsByTagName('input')[1].title = "Bing 搜索";
+
+            // change shortcut icon
+            document.getElementsByTagName('meta')[0].content = "https://raw.githubusercontent.com/Ynjxsjmh/Tampermonkey/master/image/bing-icon.ico";
+            var new_link = document.createElement("link");
+            new_link.href = "https://raw.githubusercontent.com/Ynjxsjmh/Tampermonkey/master/image/bing-icon.ico";
+            new_link.rel = "shortcut icon";
+            document.head.appendChild(new_link);
+        }
+    }
 
     function getImgSize(elLogo){
         var elImg=elLogo.querySelector("img");
