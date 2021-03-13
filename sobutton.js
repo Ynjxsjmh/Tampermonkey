f0595d08725efdee2a61c9550df39fd2e6993b34
Ynjxsjmh <ynjxsjmh@gmail.com>
2021-03-13 22:45:55 +0800

    [sobutton] Bring back SO homepage button

---
+++ sobutton.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Bring back SO homepage button
+// @namespace    ynjxsjmh.web.utils
+// @version      0.1
+// @description  Bring back SO homepage button
+// @author       Ynjxsjmh
+// @match        https://stackoverflow.com/
+// @match        https://stackoverflow.com/?tab=*
+// @match        https://*.stackexchange.com
+// @match        https://*.stackexchange.com/?tab=*
+// @icon         https://www.google.com/s2/favicons?domain=stackoverflow.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    var btnGroup = document.getElementsByClassName('s-btn-group')[0];
+    var btns = btnGroup.getElementsByTagName('a');
+
+    for (var tab of btns) {
+        const targetUrl = `https://${window.location.hostname}/?tab=${tab.getAttribute('data-value')}`;
+
+        tab.onclick = function() {
+            window.location = targetUrl;
+            return false;
+        };
+    }
+})();
