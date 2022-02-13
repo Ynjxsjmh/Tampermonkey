a97b9715d28816aa7f65020a5e642aea299ec2e4
Ynjxsjmh <ynjxsjmh@gmail.com>
2022-02-13 14:14:05 +0800

    [EasyCopy] Add more info to v2ex

--- EasyCopy.js
+++ EasyCopy.js
@@ -84,7 +84,12 @@ function v2ex() {
     btn.addEventListener('click', copyText, false);
 
     if (anchor) {
-      btn.copiedText = anchors[i].querySelector('.reply_content').innerText;
+      var author = anchors[i].querySelector('.dark').innerText;
+      var date = anchors[i].querySelector('.ago').innerText;
+      var like = anchors[i].querySelector('.small') ? ('❤' + anchors[i].querySelector('.small').innerText.trim()) : '';
+      var floor = anchors[i].querySelector('.fr .no').innerText;
+      var content = anchors[i].querySelector('.reply_content').innerText;
+      btn.copiedText = `${author}\t${date}\t${like}\t#${floor}\n${content}`;
       anchor.parentNode.insertBefore(btn, anchor.nextSibling);
     }
   }
