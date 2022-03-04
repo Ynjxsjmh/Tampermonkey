a26fa9d303053d68f9012186f1834c9f3f986998
Ynjxsjmh <ynjxsjmh@gmail.com>
2022-03-04 11:11:45 +0800

    [EasyCopy] Support adding button to new loaded zhihu answer

--- EasyCopy.js
+++ EasyCopy.js
@@ -9,6 +9,7 @@
 // @match        *.v2ex.com/t/*
 // @match        *.douban.com/group/topic/*
 // @match        *.zhihu.com/question/*
+// @require      https://code.jquery.com/jquery-3.6.0.slim.min.js
 // @grant        none
 // ==/UserScript==
 
@@ -55,6 +56,87 @@ function fallbackCopyText(text) {
     textArea.remove();
   });
 }
+
+/*--- waitForKeyElements():  A utility function, for Greasemonkey scripts,
+    that detects and handles AJAXed content.
+    https://stackoverflow.com/a/8283815/10315163
+*/
+function waitForKeyElements (
+    selectorTxt,    /* Required: The jQuery selector string that
+                        specifies the desired element(s).
+                    */
+    actionFunction, /* Required: The code to run when elements are
+                        found. It is passed a jNode to the matched
+                        element.
+                    */
+    bWaitOnce,      /* Optional: If false, will continue to scan for
+                        new elements even after the first match is
+                        found.
+                    */
+    iframeSelector  /* Optional: If set, identifies the iframe to
+                        search.
+                    */
+) {
+    var targetNodes, btargetsFound;
+
+    if (typeof iframeSelector == "undefined")
+        targetNodes     = $(selectorTxt);
+    else
+        targetNodes     = $(iframeSelector).contents ()
+                                           .find (selectorTxt);
+
+    if (targetNodes  &&  targetNodes.length > 0) {
+        btargetsFound   = true;
+        /*--- Found target node(s).  Go through each and act if they
+            are new.
+        */
+        targetNodes.each ( function () {
+            var jThis        = $(this);
+            var alreadyFound = jThis.data ('alreadyFound')  ||  false;
+
+            if (!alreadyFound) {
+                //--- Call the payload function.
+                var cancelFound     = actionFunction (jThis);
+                if (cancelFound)
+                    btargetsFound   = false;
+                else
+                    jThis.data ('alreadyFound', true);
+            }
+        } );
+    }
+    else {
+        btargetsFound   = false;
+    }
+
+    //--- Get the timer-control variable for this selector.
+    var controlObj      = waitForKeyElements.controlObj  ||  {};
+    var controlKey      = selectorTxt.replace (/[^\w]/g, "_");
+    var timeControl     = controlObj [controlKey];
+
+    //--- Now set or clear the timer as appropriate.
+    if (btargetsFound  &&  bWaitOnce  &&  timeControl) {
+        //--- The only condition where we need to clear the timer.
+        clearInterval (timeControl);
+        delete controlObj [controlKey]
+    }
+    else {
+        //--- Set a timer, if needed.
+        if ( ! timeControl) {
+            timeControl = setInterval ( function () {
+                    waitForKeyElements (    selectorTxt,
+                                            actionFunction,
+                                            bWaitOnce,
+                                            iframeSelector
+                                        );
+                },
+                300
+            );
+            controlObj [controlKey] = timeControl;
+        }
+    }
+    waitForKeyElements.controlObj   = controlObj;
+}
+
 
 function cool18() {
   const anchor = document.querySelector('button');
@@ -236,34 +318,43 @@ function douban () {
 }
 
 function zhihuQuestion() {
-  const answers = document.querySelectorAll('#QuestionAnswers-answers .List-item');
+  const addButton = () => {
+    const answers = document.querySelectorAll('#QuestionAnswers-answers .List-item');
 
-  for (var i = 0; i < answers.length; i++){
-    var btn = document.createElement('button');
-    btn.innerHTML = 'Copy';
-    btn.setAttribute('id', `copyText${i}`);
-    btn.setAttribute('class', 'Button Button--plain');
-    btn.addEventListener('click', copyText, false);
-    btn.style.display = 'none';
+    for (var i = 0; i < answers.length; i++) {
+      if ( answers[i].querySelector(`#copyText${i}`) ) {
+        continue;
+      }
 
-    answers[i].onmouseover = function() {
-      this.getElementsByTagName('button')[0].style.display = 'inline';
-    };
+      var btn = document.createElement('button');
+      btn.innerHTML = 'Copy';
+      btn.setAttribute('id', `copyText${i}`);
+      btn.setAttribute('class', 'Button Button--plain');
+      btn.addEventListener('click', copyText, false);
+      btn.style.display = 'none';
 
-    answers[i].onmouseout = function() {
-      this.getElementsByTagName('button')[0].style.display = 'none';
-    };
+      answers[i].onmouseover = function() {
+        this.getElementsByTagName('button')[0].style.display = 'inline';
+      };
 
-    var author = answers[i].querySelector('.AuthorInfo-name').textContent;
-    var date = answers[i].querySelector('.ContentItem-time span').getAttribute('data-tooltip');
-    var like = answers[i].querySelector('.VoteButton--up').getAttribute('aria-label').trim();
-    var link = answers[i].querySelector('.ContentItem-time a').href;
-    var content = answers[i].querySelector('.RichContent-inner .RichText').innerText;
-    btn.copiedText = `${author}\t${date}\t${like}\n${content}\n\n原文链接: ${link}`;
+      answers[i].onmouseout = function() {
+        this.getElementsByTagName('button')[0].style.display = 'none';
+      };
 
-    const anchor = answers[i].querySelector('.AnswerItem-authorInfo .AuthorInfo');
-    anchor.parentNode.insertBefore(btn, anchor.nextSibling);
-  }
+      var author = answers[i].querySelector('.AuthorInfo-name').textContent;
+      var date = answers[i].querySelector('.ContentItem-time span').getAttribute('data-tooltip');
+      var like = answers[i].querySelector('.VoteButton--up').getAttribute('aria-label').trim();
+      var link = answers[i].querySelector('.ContentItem-time a').href;
+      var content = answers[i].querySelector('.RichContent-inner .RichText').innerText;
+      btn.copiedText = `${author}\t${date}\t${like}\n${content}\n\n原文链接: ${link}`;
+
+      const anchor = answers[i].querySelector('.AnswerItem-authorInfo .AuthorInfo');
+      anchor.parentNode.insertBefore(btn, anchor.nextSibling);
+    }
+  };
+
+  addButton();
+  waitForKeyElements("#QuestionAnswers-answers .List-item", addButton);
 }
 
 function addBtn() {
